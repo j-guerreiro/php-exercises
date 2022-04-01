605ee46983d7f82319f49ca67261bd09bd820096
j-guerreiro <joel.borghi@gmail.com>
2022-03-31 22:03:15 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Algoritmos e Exercícios em PHP
---
+++ ex1.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Autor: Joel Guerreiro
+ * Aprendendo a sintaxe de PHP
+*/
+
+
+
+  $msg = "<h1>GATINHOS SELVAGENS DA TERRA!</h1>";
+
+
+
+  for ($i=0; $i < 100 ; $i++) { 
+    echo $msg;
+  }
+
+?>
+
+
